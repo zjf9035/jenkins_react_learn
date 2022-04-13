aa7db8d3087fb0fa4eaa05fcfad895c1033f94a2
zhujifei <zhujifei.zjf@antgroup.com>
2022-04-13 18:43:46 +0800

    欢迎页面

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>欢迎来到jenkins</h2>
+      <section
+        style={{ width: "200px", height: "200px", background: "pink" }}
+      ></section>
     </div>
   );
 }
